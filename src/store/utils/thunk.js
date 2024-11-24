f890ac75c49b8c1ae8e11e971e005308ac50e7ea
Sahaya Ronaldo <sahayaronaldo398@gmail.com>
2024-11-24 19:21:07 +0530

    changes

--- src/store/utils/thunk.js
+++ src/store/utils/thunk.js
@@ -1,27 +1,52 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+import { collection, doc, getDocs, addDoc, setDoc } from "firebase/firestore";
+import db from "./firebase"; // Ensure you have Firebase initialized in a separate `firebase.js` file
 
 
- const URL_SERV = "https://sherylmart-products.onrender.com";
 
 
+//  const URL_SERV = "https://sherylmart-products.onrender.com";
 
 
-export const fetchProducts = createAsyncThunk(
-    'products/fetchProducts',
-    async({page=1,order='asc',limit=20})=>{
-        try {
-          const response = await axios.get(`${URL_SERV}/products?_page=${page}&_order=${order}&_limit=${limit}`);
-          console.log(response.data)
-          return response.data;
-        } catch (error) {   
-          console.log('Error : ',error);
-          throw error
-        }
-    }
+// export const fetchProducts = createAsyncThunk(
+//     'products/fetchProducts',
+//     async({page=1,order='asc',limit=20})=>{
+//         try {
+//           const response = await axios.get(`${URL_SERV}/products?_page=${page}&_order=${order}&_limit=${limit}`);
+//           console.log(response.data)
+//           return response.data;
+//         } catch (error) {   
+//           console.log('Error : ',error);
+//           throw error
+//         }
+//     }
  
-)
+// )
+
+
+const productsCollectionRef = collection(db, "products");
+
+// Thunk to Fetch Products
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async (_, { rejectWithValue }) => {
+    try {
+      const querySnapshot = await getDocs(productsCollectionRef);
+      const products = [];
+      querySnapshot.forEach((doc) => {
+        products.push({ id: doc.id, ...doc.data() });
+      });
+      console.log("Products fetched:", products);
+      return products; // Return an array of product objects
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 
  export const AddToCartPage=createAsyncThunk(
   // slicename/action name
